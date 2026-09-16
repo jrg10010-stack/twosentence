b8bf094b11a7dfaf8b27913154a56c28773c8bf4
jrg10010-stack <jrg10010@nyu.edu>
2026-09-15 22:30:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let camButton = document.getElementById("cam-button");
+let imageTag = document.getElementById('my-image');
+
+let buttonName = "Camera";
+camButton.textContent = buttonName;    
+let mySound = new Audio('path/to/sound.wav');
+mySound.play();
+
+let currentPic = 0;
+let currentAudio = null;
+
+let pictures = [
+        { image: 'FrontYard.jpg', sound: 'OutsideNight.wav' },
+        { image: 'parkingLot.jpg', sound: 'OutsideNight.wav' },
+        { image: 'Kitchen.jpg', sound: 'Inside.wav' },
+        { image: 'LivingRoom.jpg', sound: 'Inside.wav' },
+        { image: 'scaryGuy.jpg', sound: 'Static.wav' },
+        { image: 'scaryGuy.jpg', sound: 'Static.wav' }
+        
+];
+
+camButton.addEventListener("click", function(){
+        if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+        }
+        buttonName = "Camera["+ (currentPic + 1) + "]";
+        camButton.textContent = buttonName;
+        currentPic += 1;
+        imageTag.src = pictures[currentPic-1].image;
+
+        currentAudio = new Audio(pictures[currentPic-1].sound);
+        currentAudio.play();
+
+        if (currentPic == 5){
+        alert("ERROR");      
+        }
+        if (currentPic == 6){
+        window.location.href = "about:blank";    
+        }
+
+        updateCam();
+});
